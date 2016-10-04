0b7c56d8d24df9c5a5e201b04640b5f62f30d098
Isadora Dantas <isadoramariadasilvadantas@gmail.com>
2016-10-04 15:23:43 -0300

    telas iniciais

---
+++ banco.php
@@ -0,0 +1,14 @@
+<?php
+    $bdServidor = '127.0.0.1';
+    $bdUsuario = 'sistematarefa';
+    $bdSenha = 'sistema';
+    $bdBanco = 'tarefas';
+
+    $conexao = mysqli_connect($bdServidor, $bdUsuario, $bdSenha, $bdBanco);
+
+    if(mysqli_connect_errno($conexao)){
+        echo "Problemas para conectar no banco. Verifique os dados!";
+        die();
+    }
+
+?>
